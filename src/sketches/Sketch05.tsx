fb703b3472041c96b81274cbd1fdc43a3dd1961f
Jon Wilson <2jonwilson@gmail.com>
2024-07-22 15:26:07 -0700

    tinier ninjas; better camera position

--- src/sketches/Sketch05.tsx
+++ src/sketches/Sketch05.tsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import {  Physics, Triplet, useCylinder, useSphere } from '@react-three/cannon'
-import { Edges, Environment, Outlines } from '@react-three/drei'
+import { Edges, Environment, OrthographicCamera, Outlines } from '@react-three/drei'
 import { Camera, TextureLoader, Vector3 } from 'three'
 import React, { useRef } from 'react';
 
@@ -25,11 +25,12 @@ function Trampoline(props: any) {
 }
 
 function Ninja(props: any) {
+  const s = 0.5;
   const [ref,api] = useSphere(() => ({
     mass: 1, 
     position: [0, 0, 0], ...props ,
     material: {restitution:1},
-    args: [0.5,10]
+    args: [s/2,10]
 
   }))
   const texture = useLoader(TextureLoader,'/ninja.01-filled-transparent.png');
@@ -44,7 +45,7 @@ function Ninja(props: any) {
         api.velocity.set(0,5,0);
       }}
     >
-      <planeGeometry args={[1,1]}/>
+      <planeGeometry args={[s,s]}/>
       <meshStandardMaterial map={texture} transparent={true} side={2}/>
     </mesh>
   )
@@ -81,8 +82,9 @@ export function Sketch05(){
       onCreated={e => {
         console.log(e.camera);
         cameraRef.current = e.camera;
+        e.camera.lookAt(new Vector3(0,3,0))
       }}
-      camera={{position: [0,1,7]}}
+      camera={{position: [0,0.5,7]}}
       onClick={e => {
         if(!cameraRef.current){
           return;
@@ -95,6 +97,7 @@ export function Sketch05(){
         setExtraNinjas([...extraNinjas, [position.x, position.y, position.z]]);
       }}
     >
+      <OrthographicCamera />
       <Environment preset="sunset" />
       <directionalLight color={"white"} position={[0,20,5]}/>
 
