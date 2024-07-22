22bfdb6d0fec3b089c6286c04c88cf4e2d87d0b3
Jon Wilson <2jonwilson@gmail.com>
2024-07-22 15:07:57 -0700

    click to spawn a ninja

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { Link } from "react-router-dom";
+import { Sketch05 } from "./sketches/Sketch05";
 
 export function App(){
   return (
-    <div className="p-4">
-      <div>
-        The Tiny Ninja Toy will appear here when it's ready.
-      </div>
-      <div>
-        <img src="/ninja.01-filled.png" width={60}/>
-      </div>
-      <div>
-        <Link to="/sketches" className="underline">Sketches</Link>
-      </div>
-    </div>
+    <Sketch05/>
   );
 }
--- src/sketches/Sketch05.tsx
+++ src/sketches/Sketch05.tsx
@@ -1,21 +1,25 @@
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { Physics, usePlane, useSphere } from '@react-three/cannon'
+import {  Physics, Triplet, useCylinder, useSphere } from '@react-three/cannon'
 import { Edges, Environment, Outlines } from '@react-three/drei'
-import { TextureLoader } from 'three'
+import { Camera, TextureLoader, Vector3 } from 'three'
+import React, { useRef } from 'react';
 
 function Trampoline(props: any) {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
+  const r = 5;
+  const args: [number,number,number,number] = [r, r, 0.05, 100];
+  const [ref] = useCylinder(() => ({
     material: {restitution:1},
     ...props,
-    type:'Dynamic'
+    type:'Kinematic',
+    args
   }))
+  
   return (
     <mesh ref={ref as any} receiveShadow castShadow>
 
       <Edges linewidth={2} threshold={15} color={"black"} />
       <Outlines thickness={0.01} color={"#c02040" } />
-      <circleGeometry args={[5, 100]} />
+      <cylinderGeometry args={args} />
     </mesh>
   )
 }
@@ -31,26 +35,81 @@ function Ninja(props: any) {
   const texture = useLoader(TextureLoader,'/ninja.01-filled-transparent.png');
   useFrame(() => ref.current!.rotation.x += 0.01)
   return (
-    <mesh ref={ref as any} castShadow receiveShadow onClick={() =>{
-      api.velocity.set(0,5,0);
-    }}>
+    <mesh
+      ref={ref as any}
+      castShadow
+      receiveShadow
+      onClick={e =>{
+        e.stopPropagation();
+        api.velocity.set(0,5,0);
+      }}
+    >
       <planeGeometry args={[1,1]}/>
       <meshStandardMaterial map={texture} transparent={true} side={2}/>
     </mesh>
   )
 }
 
+function mouseEventToWorldSpace(
+  canvas: HTMLCanvasElement,
+  camera: Camera,
+  e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  targetZ: number
+){
+  //adapted from: https://stackoverflow.com/a/13091694
+
+  const vector = new Vector3(
+    e.clientX / canvas.width * 2 - 1,
+    -e.clientY / canvas.height * 2 + 1,
+    0.5);
+  vector.unproject(camera);
+  vector.sub(camera.position).normalize();
+  const distance = (targetZ - camera.position.z) / vector.z;
+  const position = new Vector3();
+  position.copy(camera.position).add(vector.multiplyScalar(distance));
+  return position;
+}
+
 export function Sketch05(){
+  const [extraNinjas,setExtraNinjas] = React.useState<Triplet[]>([
+    [0,2,3]
+  ]);
+  const cameraRef = useRef<Camera>();
   return (
-    <Canvas camera={{position: [0,1,7]}}>
+    <>
+    <Canvas
+      onCreated={e => {
+        console.log(e.camera);
+        cameraRef.current = e.camera;
+      }}
+      camera={{position: [0,1,7]}}
+      onClick={e => {
+        if(!cameraRef.current){
+          return;
+        }
+        const camera = cameraRef.current;
+        const canvas = (e.target as HTMLCanvasElement);
+
+        const position = mouseEventToWorldSpace(canvas, camera, e, 3);
+        console.log(position);
+        setExtraNinjas([...extraNinjas, [position.x, position.y, position.z]]);
+      }}
+    >
       <Environment preset="sunset" />
       <directionalLight color={"white"} position={[0,20,5]}/>
+
       <Physics>
-        <Ninja position={[-3,3,3]} velocity={[0.5,0,0]}/>
-        <Ninja position={[0,2,3]}/>
-        <Ninja position={[3,1,3]}/>
-        <Trampoline position={[0,0,0]}/>
+        {/*<Debug color="black" scale={1.1}>*/}
+          <Ninja position={[-3,3,3]} velocity={[0.5,0,0]}/>
+          <Ninja position={[3,1,3]} velocity={[-0.1,0,0]}/>
+          {extraNinjas.map((position,i) => 
+            <Ninja position={position} key={i}/>
+          )}
+          <Trampoline position={[0,0,0]}/>
+        {/*</Debug>*/}
       </Physics>
     </Canvas>
+
+    </>
   );
 }
