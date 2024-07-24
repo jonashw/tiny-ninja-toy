c9a9e784c5650b037804fac6a07c770881dd5b12
Jon Wilson <2jonwilson@gmail.com>
2024-07-24 13:10:08 -0700

    trampoline legs

--- src/entities/Trampoline.tsx
+++ src/entities/Trampoline.tsx
@@ -5,7 +5,12 @@ import { TextureLoader } from "three";
 
 export function Trampoline(props: any) {
   const r = 5;
-  const args: [number,number,number,number] = [r, r, 0.05, 100];
+  const r_leg = 0.02;
+  const surfaceThickness = 0.05;
+  const legCount = 100;
+  const legHeight = 0.25;
+  const args: [number,number,number,number] = [r, r, surfaceThickness, 100];
+  const legArgs: [number,number,number,number] = [r_leg, r_leg, legHeight, 10];
   const texture = useLoader(TextureLoader,'/texture/trampoline.png');
   const [ref] = useCylinder(() => ({
     material: {restitution:1},
@@ -20,5 +25,20 @@ export function Trampoline(props: any) {
       <meshStandardMaterial  map={texture}/>
       <cylinderGeometry args={args} />
     </mesh>
+    {Array(legCount).fill(undefined).map((_,i) => 
+      <group rotation={[0,(i / legCount)*2*Math.PI,0]}>
+        <mesh
+          key={i}
+          position={[
+            0,
+            -(legHeight + surfaceThickness)/2,
+            r - r_leg
+          ]}
+        >
+          <meshStandardMaterial color={"#666666"}/>
+          <cylinderGeometry args={legArgs} />
+        </mesh>
+      </group>
+    )}
   </>;
 }
